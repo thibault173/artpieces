01629fc956aeb5ce785b87f399dd2a1574020f8f
thibaultvillard <thibault.villard@gmail.com>
2018-08-20 19:08:07 +0200

    Work at date

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,9 +6,16 @@ class PiecesController < ApplicationController
   end
 
   def new
+    @piece = Piece.new
   end
 
   def create
+    @piece = Piece.new(piece_params)
+    if @piece.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +25,11 @@ class PiecesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:name, :description, :price)
   end
 end
