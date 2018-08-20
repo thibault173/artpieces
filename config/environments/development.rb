f233daa58213b31f6c9c8d49bd386933403c4b67
thibaultvillard <thibault.villard@gmail.com>
2018-08-20 15:04:41 +0200

    Adding gem devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
@@ -37,6 +39,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
