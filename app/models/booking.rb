9222c28e373e7f4ce9148464f49bcc158d4f5204
thibaultvillard <thibault.villard@gmail.com>
2018-08-20 17:55:07 +0200

    Adding validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :piece
+
+  validates :start_on, :end_on, :user, :piece, presence: true
+  validates :end_on, presence: true
+  validates :status, default: "available"
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  has_many :pieces, through: :piece_tags
+
 end
