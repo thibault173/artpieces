ad5e590334575425ab095d78c2b692aaa78cb3fe
thibaultvillard <thibault.villard@gmail.com>
2018-08-21 16:00:29 +0200

    Adding everthing to have a picture (model, db, uploader) using cloudinary & carrierwave

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,8 +1,6 @@
 class Piece < ApplicationRecord
   belongs_to :user
-
-
-   mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,5 +3,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
+  def public_id
+    "artpieces/" + model.name
+  end
+
 
 end
