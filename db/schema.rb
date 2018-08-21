810d58d43869984b13c131aa8d855abafef90df0
Sarah_Lauren <s.lauren.contact@gmail.com>
2018-08-21 15:13:09 +0200

    test seed for cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_132338) do
+ActiveRecord::Schema.define(version: 2018_08_20_155539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.string "status"
+    t.string "status", default: "available"
     t.date "start_on"
     t.date "end_on"
     t.bigint "user_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Piece.create(name:'Joconde', description: "Leonard Da Vinci", price: 450, user_id: 1)
+Piece.create(name:'Enlevement de proserpine', description: "Le Bernin", price: 10 000, user_id: 1)
+
