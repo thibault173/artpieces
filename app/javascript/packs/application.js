afd5c98709b0e962304a118f86c427f9c6385ee7
christine royer <royerchrist@gmail.com>
2018-08-24 15:43:50 +0200

    selectize-multiselect

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import Choices from "choices.js";
+import "choices.js/assets/styles/css/choices.css";
+
+const tagsSelect = document.querySelector("#piece_tag_ids");
+
+if (tagsSelect) {
+  new Choices("#piece_tag_ids", {
+    removeItems: true,
+    removeItemButton: true,
+  });
+}
